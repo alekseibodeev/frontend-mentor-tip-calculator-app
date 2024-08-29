d9f8ad50ae1b278cdd3870b6849530ba992691cc
Aleksei Bodeev <bodeevaleksei@gmail.com>
2024-08-29 09:46:27 +0500

    Add script

---
+++ index.js
@@ -0,0 +1,49 @@
+const tipCustom = document.getElementById("tip-custom");
+const tipCustomValue = document.getElementById("tip-custom-value");
+
+const bill = document.getElementById("bill");
+const tip = document.getElementById("tip");
+const people = document.getElementById("people");
+const form = document.getElementById("calculator-form");
+
+const tipValue = document.getElementById("tip-value");
+const totalValue = document.getElementById("total-value");
+
+const handleInputChange = () => new FormData(form);
+
+const calcTip = (formdata) => {
+  if (!formdata.people || !formdata.bill || !formdata.tip) return 0;
+  const bill = parseFloat(formdata.bill);
+  const tip = parseFloat(formdata.tip);
+  const people = parseFloat(formdata.people);
+  return (bill * (tip / 100)) / people;
+};
+
+const calcTotal = (formdata) => {
+  if (!formdata.people || !formdata.bill) return 0;
+  if (!formdata.tip) bill / people;
+  const bill = parseFloat(formdata.bill);
+  const tip = parseFloat(formdata.tip);
+  const people = parseFloat(formdata.people);
+  return (bill * ((100 + tip) / 100)) / people;
+};
+
+tipCustom.addEventListener("input", () => {
+  tipCustomValue.focus();
+});
+
+tipCustomValue.addEventListener("input", () => {
+  tipCustom.value = tipCustomValue.value;
+});
+
+bill.addEventListener("input", handleInputChange);
+
+tip.addEventListener("input", handleInputChange);
+
+people.addEventListener("input", handleInputChange);
+
+form.addEventListener("formdata", (event) => {
+  const formdata = Object.fromEntries(event.formData);
+  tipValue.textContent = calcTip(formdata).toFixed(2);
+  totalValue.textContent = calcTotal(formdata).toFixed(2);
+});
